60a206c33db59e44c204198f8fd17fbe30ee8832
aesthetic <164383159+V0lsheBstvo@users.noreply.github.com>
2025-05-11 11:20:39 +0300

    Update TsT_ui.lua

--- TsT_ui.lua
+++ TsT_ui.lua
@@ -3208,16 +3208,9 @@ function lu_ui.CreateLib(lu_Name, themeList)
                     Utility:TweenObject(label, {TextColor3 = Color3.fromRGB(255, 255, 255)}, 0.2)
                 end
 
-                coroutine.wrap(
-                    function()
-                        while wait() do
-                            label.BackgroundColor3 = themeList.SchemeColor
-                            label.TextColor3 = themeList.TextColor
-                        end
-                    end
-                )()
                 updateSectionFrame()
                 UpdateSize()
+                
                 function labelFunctions:UpdateLabel(newText)
                     if label.Text ~= "  " .. newText then
                         label.Text = "  " .. newText
