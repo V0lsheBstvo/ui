460d6c1a76e40a225b79e69c1b34c7d6a4263e9d
aesthetic <164383159+V0lsheBstvo@users.noreply.github.com>
2025-05-13 12:33:08 +0300

    Create HowToUse.lua

---
+++ HowToUse.lua
@@ -0,0 +1,12 @@
+
+local notif = loadstring(game:HttpGet("https://raw.githubusercontent.com/V0lsheBstvo/ui/refs/heads/main/notiflib.lua"))()
+
+notif:Notification("Example Title","Example Description","GothamSemibold","Gotham",5) -- title: <string> description: <string> title font: <string> description font: <string> notification show time: <number>
+
+--[[
+PARAM 1: title: <string>
+PARAM 2: description: <string>
+PARAM 3: title font: <string>
+PARAM 4: description font: <string>
+PARAM 5: notification appearance time: <number>
+--]]
